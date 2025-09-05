91637679731a61c3999f68bcaf7d880d6311a096
bot SAM <128407982+TAHKInteractingAI@users.noreply.github.com>
2025-09-05 15:35:07 +0700

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -76,7 +76,7 @@ def send_message(driver, message):
 def login():
     import tempfile
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
